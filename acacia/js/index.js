1418436434d0b4803e0903eca018b9c72b92a22b
Artem Perminov <perminov.artem@eurostavka.ru>
2023-02-16 22:46:51 +0300

    refactoring

---
+++ acacia/js/index.js
@@ -0,0 +1,9 @@
+// Взаимодействие с пользователем через стандартные способы в браузере
+
+// alert('Hello!');
+
+// const result = confirm('Are you happy?');
+// console.log(result);
+
+// const answer = prompt('How old are you?', '');
+// console.log(answer);
